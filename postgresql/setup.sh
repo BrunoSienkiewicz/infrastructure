e2acb27c88dcda13ae22d9fe1729d94293104a1c
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2024-03-24 15:31:05 +0100

    password typo

--- postgresql/setup.sh
+++ postgresql/setup.sh
@@ -3,14 +3,14 @@ source .env
 kubectl create ns postgresql
 
 export POSTGRES_USER=$SECRET_POSTGRES_USER
-export POSTGRES_PASS=$SECRET_POSTGRES_PASS
+export POSTGRES_PASSWORD=$SECRET_POSTGRES_PASSWORD
 export POSTGRES_DB="postgres"
 export REPLICATION_USER=$SECRET_REPLICATION_USER
 export REPLICATION_PASS=$SECRET_REPLICATION_PASS
 
 kubectl create -n postgresql secret generic postgresql \
     --from-literal=POSTGRES_USER=${POSTGRES_USER} \
-    --from-literal=POSTGRES_PASS=${POSTGRES_PASS} \
+    --from-literal=POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
     --from-literal=POSTGRES_DB=${POSTGRES_DB} \
     --from-literal=REPLICATION_USER=${REPLICATION_USER} \
     --from-literal=REPLICATION_PASS=${REPLICATION_PASS}
@@ -18,6 +18,6 @@ kubectl create -n postgresql secret generic postgresql \
 kubectl -n postgresql apply -f storage.yaml
 kubectl -n postgresql apply -f statefulset.yaml
 
-unset POSTGRES_USER POSTGRES_PASS POSTGRES_DB REPLICATION_USER REPLICATION_PASS
+unset POSTGRES_USER POSTGRES_PASSWORD POSTGRES_DB REPLICATION_USER REPLICATION_PASS
 
 
