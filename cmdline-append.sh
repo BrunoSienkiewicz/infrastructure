bca07ce45014c0dea7c4ba5e76f84f39abb2997d
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2024-03-24 11:19:14 +0100

    cmdline fix

--- cmdline-append.sh
+++ cmdline-append.sh
@@ -1,2 +1,1 @@
-echo " cgroup_memory=1 cgroup_enable=memory ip=192.168.1.24::192.168.1.1:255.255.255.0:rpimaster:eth0:off" >> /boot/cmdline.txt
-echo " cgroup_memory=1 cgroup_enable=memory ip=192.168.1.24::192.168.1.1:255.255.255.0:rpimaster:eth0:off" >> /boot/firmware/cmdline.txt
+echo "ip=192.168.1.24::192.168.1.1:255.255.255.0:rpimaster:eth0:off cgroup_memory=1 cgroup_enable=memory " >> /boot/firmware/cmdline.txt
