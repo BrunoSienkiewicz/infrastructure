f1b6976678bb6d5658d2c9c426c241a78abaa096
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2024-03-29 22:37:54 +0100

    setup script update

--- traefik/setup.sh
+++ traefik/setup.sh
@@ -2,5 +2,5 @@ kubectl create ns traefik
 
 helm repo add traefik https://traefik.github.io/charts
 helm repo update
-helm install traefik traefik/traefik --namespace=traefik --values=values.yaml --debug
+helm upgrade --install traefik traefik/traefik --namespace=traefik --values=values.yaml --debug
 
