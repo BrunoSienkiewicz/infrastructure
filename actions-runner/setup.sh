1ee70de17c3cd685798873981a6ee6fe276a5111
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2024-03-30 19:41:42 +0100

    setup script update

--- actions-runner/setup.sh
+++ actions-runner/setup.sh
@@ -10,7 +10,7 @@ else
 fi
 
 # Check if arguments are provided
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <repository_name>"
     exit 1
 fi
@@ -30,10 +30,13 @@ kubectl create secret generic "$REPO_NAME-secret" \
   --from-literal=GITHUB_OWNER="$GITHUB_OWNER" \
   --from-literal=GITHUB_REPOSITORY="$REPO_NAME" 
 
-# Update the deployment to use the secret
-kubectl patch deployment "github-runner-$REPO_NAME" \
-    --type='json' \
-    -p='[{"op": "replace", "path": "/spec/template/spec/containers/0/env/0/valueFrom/secretKeyRef/name", "value": "'"$REPO_NAME"'-secret"}]'
+sed -e "s/{{SECRET_NAME}}/$REPO_NAME-secret/g" deployment.yaml > deployment_generated.yaml
+
+# Apply the generated deployment YAML file
+kubectl apply -f deployment_generated.yaml
+
+# Clean up generated files
+rm deployment_generated.yaml
 
 unset REPO_NAME GITHUB_PERSONAL_TOKEN GITHUB_OWNER GITHUB_REPOSITORY
 
