bdef71cd41e4b0877e34c6c7e710b9eb0e37e566
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2024-04-07 14:52:57 +0200

    local provisoner

--- local-storageclass/setup.sh
+++ local-storageclass/setup.sh
@@ -1,4 +1,4 @@
 kubectl create namespace local-provisioner
 
-helm repo add sig-storage-local-static-provisioner https://kubernetes-sigs.github.io/sig-storage-local-static-provisioner
-helm template --debug sig-storage-local-static-provisioner/local-static-provisioner --version 2.6.0 --namespace local-provisioner > local-volume-provisioner.generated.yaml
+kubectl -n local-provisioner apply -f local_provisioner_storageclass.yaml
+helm install -f values.yaml -n local-provisioner localprovi ./helm/provisioner/
