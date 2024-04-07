b0efd8cd7068d1f78fea8ae4162ef87425003193
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2024-04-07 15:38:52 +0200

    helm upgrade

--- local-storageclass/setup.sh
+++ local-storageclass/setup.sh
@@ -1,4 +1,4 @@
 kubectl create namespace local-provisioner
 
-kubectl -n local-provisioner apply -f local_provisioner_storageclass.yaml
-helm install -f values.yaml -n local-provisioner localprovi ./helm/provisioner/
+kubectl -n local-provisioner apply -f local-provisioner-storageclass.yaml
+helm upgrade --install -f values.yaml -n local-provisioner localprovi ./helm/provisioner/
