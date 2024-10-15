b10645c87813f72ac0d2ca3fd77582ad1c1026aa
Bruno Sienkiewicz <bruno.sienkiewicz@gmail.com>
2024-10-15 22:18:16 +0200

    remove: unused services

--- airflow/setup.sh
+++
@@ -1,8 +0,0 @@
-kubectl create namespace airflow
-
-helm repo add apache-airflow https://airflow.apache.org
-helm repo update
-helm search repo airflow
-helm install airflow apache-airflow/airflow --namespace airflow --debug
-
-
--- local-storageclass/setup.sh
+++
@@ -1,4 +0,0 @@
-kubectl create namespace local-provisioner
-
-kubectl -n local-provisioner apply -f local-provisioner-storageclass.yaml
-helm upgrade --install -f values.yaml -n local-provisioner localprovi ./helm/provisioner/
